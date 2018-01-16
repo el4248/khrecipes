edbcb0fff3926d4a6c8292f3fffb3bfb2c347d5b
ajtoo <ajtoo.dev@gmail.com>
2018-01-16 15:51:23 -0800

    Update scraper to handle malformed html better and add movement cmds

--- scraper/scrapeKHBBSComponents.py
+++ scraper/scrapeKHBBSComponents.py
@@ -1,4 +1,3 @@
-from collections import deque
 import json
 
 from util import get_soup
@@ -6,10 +5,12 @@ from util import get_soup
 BASE_URL = "http://www.finalfantasykingdom.net/"
 ATTACKS_SUFFIX = "bbsatkcommand.php"
 MAGIC_SUFFIX = "bbsmagcommand.php"
+MOVEMENT_SUFFIX = "bbsmovecommand.php"
 
 def main():
 	get_components(BASE_URL + ATTACKS_SUFFIX, "attack")
 	get_components(BASE_URL + MAGIC_SUFFIX, "magic")
+	get_components(BASE_URL + MOVEMENT_SUFFIX, "movement")
 	return
 
 def get_components(url, componentType):
@@ -17,9 +18,10 @@ def get_components(url, componentType):
 	soup = get_soup(url)
 	attack_table_rows = soup.find_all("table")[10].find_all("tr")
 	row_iter = iter(attack_table_rows)
-	next(row_iter)
+	next(row_iter)		#ignore header row
 	for tr in row_iter:
-		if len(tr.findChildren()) > 13:	#ignore odd rows that get detected but aren't part of the chart
+		items_in_tr = len(tr.findChildren())
+		if items_in_tr > 13 or items_in_tr < 4:	#ignore odd rows that get detected but aren't part of the chart
 			continue
 		attacks.append(parse_component_row(tr, componentType))
 	print("found " + str(len(attacks)) + " " + componentType + "...")
@@ -36,7 +38,11 @@ def parse_component_row(tr, componentType):
 
 	#site uses "blank.png" in place of image if not useable by a certain character
 	users = ""
-	character_images = td[6].find_all("img")
+	if componentType == "movement":
+		character_images = td[3].find_all("img")		#on the movement page, rows are 4 <td> long
+	else:
+		character_images = td[6].find_all("img")
+		
 	if character_images[0].get("src") == "bbs/dlterra1.png" : users += "T"
 	if character_images[1].get("src") == "bbs/dlventus1.png" : users += "V"
 	if character_images[2].get("src") == "bbs/dlaqua1.png" : users += "A"
--- scraper/util.py
+++ scraper/util.py
@@ -1,7 +1,8 @@
 import bs4
+import html5lib
 from urllib.request import urlopen
 
 def get_soup(url):
     page = urlopen(url)				# get page to soupify
-    soup = bs4.BeautifulSoup(page, "html.parser")
+    soup = bs4.BeautifulSoup(page, "html5lib")
     return soup
