4381492549a01363f4cf7432999cc7a62d667d60
ajtoo <ajtoo.dev@gmail.com>
2018-02-14 23:56:08 -0800

    Create store and implement actions and reducers for the search endpoint

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
+
   const root = document.getElementById('app');
-  window.axios = axios;
-  ReactDOM.render(<Root/>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
