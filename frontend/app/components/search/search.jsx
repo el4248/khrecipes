668aebed6942d87672985b5768b7193208235a35
ajtoo <ajtoo.dev@gmail.com>
2018-02-26 22:43:20 -0800

    Implement basic recipe display functionality

--- frontend/app/components/search/search.jsx
+++ frontend/app/components/search/search.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import RecipeCard from "./recipe_card";
 
 class SearchView extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.props.fetchRecipes("ars solum");
-    // console.log(this.props.recipes);
   }
 
   render() {
+    let recipeCards = [];
+    for(let recipe in this.props.recipes) {
+      let cardInfo = JSON.stringify(this.props.recipes[recipe]);
+      recipeCards.push(<RecipeCard key={recipe} recipe={this.props.recipes[recipe]}/>);
+    }
+
     return (
-    <div>
-      {JSON.stringify(this.props.recipes)}
-    </div>       
+      <div>
+        {recipeCards}
+      </div>       
     );
   }
 }
